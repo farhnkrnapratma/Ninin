22744f95ccb6d7a7eca3be8499e3270c38f98dd9
Reidho Satria <me@rei.my.id>
2025-05-18 16:46:51 +0700

    Reimplement Scroll Size

--- Scroll.java
+++ Scroll.java
@@ -5,7 +5,7 @@ public class Scroll extends Objects {
 
   public Scroll() {
     GreenfootImage image = new GreenfootImage("scroll.png");
-    image.scale(20, 20);
+    image.scale(15, 15);
     setImage(image);
   }
 }
